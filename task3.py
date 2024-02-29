a4faabe6a9246fbd1ce163a8c5b1ce27ea9d2d1d
unknown <leoalfonso2003@gmail.com>
2024-02-29 23:26:01 +0700

    third commit

---
+++ task3.py
@@ -0,0 +1,9 @@
+def main(): 
+    number = float(input("Enter a number: "))
+    if number < 100:
+        print("Small")
+    elif number > 200:
+        print("Large")
+    else:
+        print("Medium")
+main()
